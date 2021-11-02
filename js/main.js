58f1b7aaf55cd7e7ce4fae59d75e287c61ef0ae0
Djendja <andjela.dj5@gmail.com>
2021-11-02 09:23:36 +0100

    js exercise

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let a=5;
+let b=3;
+
+var f = 77;
+
+// console.log(sum(a, b));
+//console.log(sub(a, b));
+//console.log(div(a, b));
+//console.log(mul(a, b));
+// console.log(c);
+
+
+// var sum = 6;
+
+function sum(x, y) {
+    // console.log("F =", f);
+    var f = 55;
+    return x + y; 
+}
+
+// console.log(sum(a,b));
+
+
+
+
+function sub(x, y) {
+    return x - y; 
+}
+
+function div(x, y) {
+    return x / y; 
+}
+
+function mul(x, y) {
+    return x * y; 
+}
+
+let arrowFunction =  a  =>  {
+  return a + b;
+}
+console.log(arrowFunction(a));
+
+
